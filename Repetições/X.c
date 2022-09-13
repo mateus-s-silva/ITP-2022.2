f45f00f23f9871c7fde803f27b121a53a438f87a
Mateus Sousa <mattahyah@gmail.com>
2022-09-12 21:40:54 -0300

    nao sei mais de nada

--- Repetições/X.c
+++ Repetições/X.c
@@ -5,7 +5,7 @@ int main(){
     char letra;
     scanf("%d %c", &num, &letra);
     num--;
-    for(linha = 0; linha < coluna; linha++){
+    for(linha = 0; linha < num; linha++){
         for(coluna = 0; coluna < num; coluna++){
             if(linha == coluna || ((linha+coluna) == (num-1))){
             printf("%c", letra);
